f60ec3dbf7ccfebaa032d952810c8ebffc5e5c0d
黄树楠 <huangshunan@NMacBook-Pro.local>
2026-04-03 16:06:47 +0800

    新增用户登录与管理功能-2

--- server.ts
+++ server.ts
@@ -24,7 +24,7 @@ const __dirname = path.dirname(__filename);
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = 7786;
 
   app.use(express.json());
   app.use(cookieParser());
